53c6f62e8f34d4d76afed83d4e6bfa811cd403f5
Sajalhowlader <sajolhowlader08@gmail.com>
2022-06-30 19:05:59 +0600

    add database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -12,6 +13,23 @@ app.get('/', (req,res)=>{
     res.send("Running")
 })
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qjk5i.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async() =>{
+try{
+await client.connect();
+console.log("connect");
+const taskCollection = client.db("endGame").collection('tasks')
+
+
+}
+finally{
+
+}
+}
+run().catch(console.dir)
 app.listen(port,()=>{
     console.log("port is running on ",port)
 })
